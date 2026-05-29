d8cb7914f92b26b52f3ea3e083e69c9a1b3142a4
Colline Lazona <colline.lazona@ftwfoundation.org>
2026-05-29 09:30:12 +0800

    Update config.py

--- 02_PIPELINE_AND_CODE/utilities/config.py
+++ 02_PIPELINE_AND_CODE/utilities/config.py
@@ -3,11 +3,16 @@
 # =====================================================
 
 # Global Settings
-PROJECT_ID = "YOUR_PROJECT_ID" # <--- Modify - Click your Project. Copy the ID
+PROJECT_ID = "fgh-ds-practical-497509" # <--- Modify - Click your Project. Copy the ID
 LOCATION = "us"
-PROCESSOR_ID = "YOUR_DOCUMENT_AI_PROCESSOR_ID" # <--- Modify
+PROCESSOR_ID = "2f71eeb60480ad05" # <--- Modify
 BUCKET_NAME = f"{PROJECT_ID}-tax-pipeline"
-DATASET_ID = "tax_pipeline_db"
+# DATASET_ID = "tax_pipeline_db"
+
+# NEW
+BRONZE_DATASET = "bronze_raw_extractions"
+SILVER_DATASET = "silver_clean_transformations"
+GOLD_DATASET   = "gold_mart_crm"
 
 # Local Paths
 LOCAL_DATA_DIR = "01_DATA_SOURCES/sampling_input_docs"
